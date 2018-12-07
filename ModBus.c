14718e0b55cbe874911cf8c419d5de9467abe993
Hillerr <38991431+Hillerr@users.noreply.github.com>
2018-12-07 08:54:04 -0200

    modbus functions

--- ModBus.c
+++ ModBus.c
@@ -6,3 +6,42 @@
  */
 
 
+#include <stdio.h>
+
+
+uint16_t CRC16_2(uint8_t *buf, int len) {
+
+	uint32_t crc = 0xFFFF;
+
+	int i;
+
+	for (i = 0; i < len; i++) {
+
+		crc ^= (uint16_t) buf[i]; // XOR byte into least sig. byte of crc
+
+		for (int i = 8; i != 0; i--) { // Loop over each bit
+
+			if ((crc & 0x0001) != 0) { // If the LSB is set
+
+				crc >>= 1; // Shift right and XOR 0xA001
+				crc ^= 0xA001;
+
+			} else
+				// Else LSB is not set
+				crc >>= 1; // Just shift right
+		}
+	}
+	return crc;
+}
+
+uint8_t *create_buffer(uint8_t addr, uint8_t cmd, uint16_t reg, uint16_t data){
+
+	uint8_t buffer[8], mask = 0xFF;
+	uint16_t crc = 0;
+
+	buffer[0] = addr;
+	buffer[1] = cmd;
+	buffer[2] = (reg>>8);
+	buffer[3] = reg | mask;
+	return buffer;
+}
