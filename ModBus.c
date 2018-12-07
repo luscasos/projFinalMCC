dcb85dc3a2aa8e89afcc4f68aa96caa66bca9580
Hillerr <38991431+Hillerr@users.noreply.github.com>
2018-12-07 08:35:32 -0200

    ModBus module

---
+++ ModBus.c
@@ -0,0 +1,8 @@
+/*
+ * ModBus.c
+ *
+ *  Created on: 7 de dez de 2018
+ *      Author: rafae
+ */
+
+
---
+++ ModBus.h
@@ -0,0 +1,13 @@
+/*
+ * ModBus.h
+ *
+ *  Created on: 7 de dez de 2018
+ *      Author: rafae
+ */
+
+#ifndef MODBUS_H_
+#define MODBUS_H_
+
+
+
+#endif /* MODBUS_H_ */
